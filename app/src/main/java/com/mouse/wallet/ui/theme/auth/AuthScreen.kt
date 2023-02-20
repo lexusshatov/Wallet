c585881515be18d5be14c1614d9e2f3f850c6bdc
Oleh Shatov <lexusshatov2@gmail.com>
2023-02-20 12:33:06 +0200

    Update gradle

--- app/src/main/java/com/mouse/wallet/ui/theme/auth/AuthScreen.kt
+++ app/src/main/java/com/mouse/wallet/ui/theme/auth/AuthScreen.kt
@@ -5,11 +5,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -20,6 +17,9 @@ import com.mouse.wallet.R
 @Composable
 fun AuthScreen() {
     var login: String by rememberSaveable { mutableStateOf("") }
+    var loginError: String by remember {
+        mutableStateOf("")
+    }
     var password: String by rememberSaveable { mutableStateOf("") }
 
     Column(
--- core/src/main/java/com/mouse/core/validate/login/LoginValidate.kt
+++
@@ -1,8 +0,0 @@
-package com.mouse.core.validate.login
-
-import com.mouse.core.interaction.LoginInteraction
-import com.mouse.core.validate.BaseValidate
-
-class LoginValidate : BaseValidate<LoginInteraction.Params>(
-    LengthStage(3, 30)
-)
